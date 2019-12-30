9de19f61b4810ae4f51d6ff88ff8caf94631afa9
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2019-12-30 14:53:27 -0800

    players headshots
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- players.py
+++ players.py
@@ -5,11 +5,10 @@ import pandas as pd
 
 players = requests.get('https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=Totals&Scope=S&Season=2019-20&SeasonType=Regular+Season&StatCategory=FG3M')
 data = players.json()
+print(data)
 
 with open('./data/players.json', 'w') as f:
     json.dump(data, f)
-except:
-    print(e)
 
 # Convert to .json to .csv
 df = pd.DataFrame(data['resultSet']['rowSet'], columns=data['resultSet']['headers'])
