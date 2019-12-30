992f1bd4dcf6df0b8571a68663085064a94ced9d
Finbarrs Oketunji <16977112+0xnu@users.noreply.github.com>
2019-12-30 11:15:50 -0800

    Update and rename nba-data.py to players.py

---
+++ players.py
@@ -0,0 +1,13 @@
+import json
+import requests
+import pandas as pd
+
+players = requests.get('https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=Totals&Scope=S&Season=2019-20&SeasonType=Regular+Season&StatCategory=FG3M')
+data = players.json()
+with open('./data/players.json', 'w') as f:
+    json.dump(data, f)
+
+# Convert to .json to .csv
+df = pd.DataFrame(data['resultSet']['rowSet'], columns=data['resultSet']['headers'])
+df.to_csv('./data/players.csv', index=False)
+print('I am done scraping!')
