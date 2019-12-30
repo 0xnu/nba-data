3069cbc6a93a2a67bc11673150b20e8699d4758e
Finbarrs Oketunji <16977112+0xnu@users.noreply.github.com>
2019-12-30 11:19:38 -0800

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p ./player_data
-python3 ./nba-data.py
+python3 ./players.py
 find player_data -name '*.json' -exec cat {} \; | jq --slurp . -c > players.json
 #mkdir -p ./data; mv players.json $_
 mkdir -p ./data; mv players.json ./data
