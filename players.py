1507b4d38bfe9a1c8ec0484d3d603e3241fb15c8
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2019-12-30 16:31:36 -0800

    json data
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- players.py
+++ players.py
@@ -1,12 +1,11 @@
-import sys
 import requests
-import ujson as json
+import simplejson as json
 import pandas as pd
 
 players = requests.get('https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=Totals&Scope=S&Season=2019-20&SeasonType=Regular+Season&StatCategory=FG3M')
 data = players.json()
-print(data)
 
+# Dump data
 with open('./data/players.json', 'w') as f:
     json.dump(data, f)
 
