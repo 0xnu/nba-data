01c6a4bd2aa9375576ccf8c9f9c5a07516484ff3
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2019-12-30 19:48:58 -0800

    refactor players.py
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- players.py
+++ players.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 19:35:35 2019
+author: Finbarrs Oketunji
+"""
+
 import requests
 import simplejson as json
 import pandas as pd
 
-players = requests.get('https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=Totals&Scope=S&Season=2019-20&SeasonType=Regular+Season&StatCategory=FG3M')
+url = 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=Totals&Scope=S&Season=2019-20&SeasonType=Regular+Season&StatCategory=FG3M'
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
+headers = {'User-Agent': user_agent}
+players = requests.get(url, headers=headers)
 data = players.json()
 
 # Dump data
@@ -12,4 +21,3 @@ with open('./data/players.json', 'w') as f:
 # Convert to .json to .csv
 df = pd.DataFrame(data['resultSet']['rowSet'], columns=data['resultSet']['headers'])
 df.to_csv('./data/players.csv', index=False)
-
