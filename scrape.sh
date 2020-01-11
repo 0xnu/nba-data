703004e9645c950b8a8165fa9dd56ec4a5713825
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2020-01-11 10:30:47 -0800

    nba data
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#chmod +x scrape.sh && ./scrape.sh
 
 for TEAM in "ATL" "BOS" "BKN" "CHA" "CHI" "CLE" "DAL" "DEN" "DET" "GSW" "HOU" "IND" "LAC" "LAL" "MEM" "MIA" "MIL" "MIN" "NOP" "NYK" "OKC" "ORL" "PHI" "PHX" "POR" "SAC" "SAS" "TOR" "UTA" "WAS"
 do
@@ -10,8 +11,11 @@ done
 mkdir -p ./data/logos/png; mkdir -p ./data/logos/svg
 mv *.png ./data/logos/png; mv *.svg ./data/logos/svg
 
-python3 ./players.py
+python3 ./players.py && python3 ./headshots.py
 
-python3 ./headshots.py
+# Push to repository
+git add .
+git commit -s -m "nba data"
+git push
 
 echo "I am done scraping data. Bye!"
